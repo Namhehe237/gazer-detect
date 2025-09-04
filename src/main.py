1f39916e692e63a606e4e261fe8a6c1e1364f0dd
Namhehe237 <namnguyen23072003@gmail.com>
2025-09-04 14:39:16 +0700

    update code

--- src/main.py
+++ src/main.py
@@ -29,6 +29,50 @@ def get_blink_ratio(eye_points, facial_landmarks):
         
         return ratio
 
+def get_gaze_ratio(eye_points, facial_landmarks) :
+        left_eye_region = np.array([(facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y),
+                                     (facial_landmarks.part(eye_points[1]).x, facial_landmarks.part(eye_points[1]).y),
+                                     (facial_landmarks.part(eye_points[2]).x, facial_landmarks.part(eye_points[2]).y),
+                                     (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y),
+                                     (facial_landmarks.part(eye_points[4]).x, facial_landmarks.part(eye_points[4]).y),
+                                     (facial_landmarks.part(eye_points[5]).x, facial_landmarks.part(eye_points[5]).y)], dtype=np.int32)
+
+        # right_eye_region = np.array([(facial_landmarks.part(eye_points[6]).x, facial_landmarks.part(eye_points[6]).y),
+        #                               (facial_landmarks.part(eye_points[7]).x, facial_landmarks.part(eye_points[7]).y),
+        #                               (facial_landmarks.part(eye_points[8]).x, facial_landmarks.part(eye_points[8]).y),
+        #                               (facial_landmarks.part(eye_points[9]).x, facial_landmarks.part(eye_points[9]).y),
+        #                               (facial_landmarks.part(eye_points[10]).x, facial_landmarks.part(eye_points[10]).y),
+        #                               (facial_landmarks.part(eye_points[11]).x, facial_landmarks.part(eye_points[11]).y)], dtype=np.int32)
+
+        # cv2.polylines(frame, [right_eye_region], True, (0,255,0), 2)
+        
+        height, width, _ = frame.shape
+        mask = np.zeros((height,width), np.uint8)
+        
+        cv2.polylines(frame, [left_eye_region], True, 255, 0)   
+        cv2.fillPoly(mask,[left_eye_region],255) 
+        left_eye = cv2.bitwise_and(gray,gray,mask=mask)
+        
+        min_x = np.min(left_eye_region[:, 0])
+        min_y = np.min(left_eye_region[:, 1])
+        max_x = np.max(left_eye_region[:, 0])
+        max_y = np.max(left_eye_region[:, 1])
+
+        gray_eye = left_eye[min_y: max_y, min_x: max_x]
+        _, threshold_eye = cv2.threshold(gray_eye, 70, 255, cv2.THRESH_BINARY)
+        
+        height, width = threshold_eye.shape
+        left_side_threshold = threshold_eye[0: height, 0 : int(width/2)]
+        left_side_white = cv2.countNonZero(left_side_threshold)
+        
+        right_side_threshold = threshold_eye[0: height,int(width/2): width]
+        right_side_white = cv2.countNonZero(right_side_threshold)
+        
+        
+        
+        gaze_ratio = left_side_white / right_side_white
+    
+        return gaze_ratio
 
 font = cv2.FONT_HERSHEY_PLAIN
 
@@ -49,9 +93,22 @@ while True:
         right_eye_ratio = get_blink_ratio([42,43,44,45,46,47], landmarks)
         
         blinking_ratio = (left_eye_ratio + right_eye_ratio)/2
-        if blinking_ratio > 5/7:
+        if blinking_ratio > 5.7:
             cv2.putText(frame, "BLINKING",(50,150),font ,7,(255,0,0))
     
+        gaze_ratio_left_eye = get_gaze_ratio([36,37,38,39,40,41], landmarks)
+        gaze_ratio_right_eye = get_gaze_ratio([42,43,44,45,46,47], landmarks)
+        gaze_ratio = (gaze_ratio_left_eye + gaze_ratio_right_eye) / 2
+        if gaze_ratio <= 0.9 :
+            cv2.putText(frame, "LEFT",(50,50),font ,2,(0,0,255),3)
+        elif 0.9 < gaze_ratio < 1.7 :
+            cv2.putText(frame, "CENTER",(50,50),font ,2,(0,0,255),3)
+        else :
+            cv2.putText(frame, "RIGHT",(50,50),font ,2,(0,0,255),3)
+            
+        # cv2.putText(frame, str(gaze_ratio_left_eye), (50,100), font, 2, (0,0,255), 3)
+        # cv2.putText(frame, str(gaze_ratio_right_eye), (50,200), font, 2, (0,0,255), 3)
+
     cv2.imshow("VIDEO", frame)
     
     key = cv2.waitKey(1)
